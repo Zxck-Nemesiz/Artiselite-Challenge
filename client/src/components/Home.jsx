a079696698b4e7187819e84e49448e6eb1dea9a0
MUHAMMAD ZAKUAN BIN ZULKARNAIN <85609731+Zxck-Nemesiz@users.noreply.github.com>
2024-10-12 17:09:17 +0900

    testing

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -32,7 +32,7 @@ const Home = ({ user }) => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    arrows: true, // Add arrows for navigation
+    arrows: true, 
     responsive: [
       {
         breakpoint: 1024,
